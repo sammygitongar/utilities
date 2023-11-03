f2f686d225705bae83d87eb6088e9aef4a14108a
Samuel Gitonga <sammygitongar@gmail.com>
2023-11-03 17:56:22 +0300

    Inserting data in a database using php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,8 +2,6 @@
     mysql connection: It is obsolete - do not use
     improved mysql(called mysqli) - has extra sql injection protection
     php data object (pdo connections) - Used in this instance -->
-
-
 <?php
 
     $dsn = "mysql:host=localhost;dbname=learning_db";
@@ -13,8 +11,6 @@
     try{
         $pdo = new PDO($dsn, $dbusername, $dbpassword);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connection Successful";
     }  catch (PDOException $e) {
         echo "Connection failed: ". $e->getMessage();
     };
-
