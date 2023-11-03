c514de22704f2c489a315d3ee83ac879d245711a
Samuel Gitonga <sammygitongar@gmail.com>
2023-11-03 10:37:58 +0300

    Addition of pdo php connection

---
+++ connection.php
@@ -0,0 +1,20 @@
+<!-- There are several ways to connect to a database:
+    mysql connection: It is obsolete - do not use
+    improved mysql(called mysqli) - has extra sql injection protection
+    php data object (pdo connections) - Used in this instance -->
+
+
+<?php
+
+    $dsn = "mysql:host=localhost;dbname=learning_db";
+    $dbusername = "root";
+    $dbpassword = "";
+
+    try{
+        $pdo = new PDO($dsn, $dbusername, $dbpassword);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connection Successful";
+    }  catch (PDOException $e) {
+        echo "Connection failed: ". $e->getMessage();
+    };
+
